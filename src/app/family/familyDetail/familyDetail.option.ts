5b45c8757be62688e0c04b959ed411c025bf5ffd
liuweiii <liuweiiii@163.com>
2017-02-16 20:37:13 +0800

    refactor

--- src/app/family/familyDetail/familyDetail.option.ts
+++ src/app/family/familyDetail/familyDetail.option.ts
@@ -17,55 +17,54 @@ export class FamilyDetailOption {
     static data: any;
     static links: any;
 
-    private static pushPerson(person: Person,
-                              category: string,
-                              me: Person,
-                              roleName: string,
-                              x: number,
-                              y: number) {
-        if (person) {
-            this.data.push({
-                name: person.name,
-                person: person,
-                category: category,
-                value: person.introduce,
-                x: x,
-                y: y
-            });
-            this.links.push({
-                source: me.name,
-                target: person.name,
-                label: {
-                    normal: {
-                        show: true,
-                        formatter: roleName
-                    }
+    private static pushData(person: Person, category: string, x: number, y: number) {
+        this.data.push({
+            name: person.name,
+            person: person,
+            category: category,
+            value: person.introduce,
+            x: x,
+            y: y
+        })
+    }
+
+    private static pushLink(person: Person, me: Person, roleName: string) {
+        this.links.push({
+            source: me.name,
+            target: person.name,
+            label: {
+                normal: {
+                    show: true,
+                    formatter: roleName
                 }
-            })
+            }
+        })
+    }
+
+    private static pushPerson(person: Person, category: string, me: Person, roleName: string, x: number, y: number) {
+        if (person) {
+            this.pushData(person, category, x, y);
+            this.pushLink(person, me, roleName);
         }
+    }
+    private static hasChild(family:Family):boolean{
+        return family.children && family.children.length > 0;
     }
 
     public static generateOption(me: Person, family: Family) {
         this.links = [];
-        this.data = [{
-            name: family.me.name,
-            person: family.me,
-            category: 'me',
-            value: family.me.introduce,
-            x: 300,
-            y: 300
-        }];
+        this.data = [];
 
+        this.pushData(family.me, 'me', 300, 300);
         this.pushPerson(family.mother, 'mother', me, "妈", 800, 300);
         this.pushPerson(family.father, 'father', me, '爸', 550, 100);
         this.pushPerson(family.spouse, 'spouse', me, '配偶', 500, 800);
 
-        if (family.children && family.children.length > 0) {
+        if (this.hasChild(family)) {
             family.children.forEach(
-                function (value: Person, index: number) {
-                    FamilyDetailOption.pushPerson(
-                        value, 'children', me,
-                        value.six == "male" ? "儿子" : "女儿", 0, 200 + index * 150);
+                function (child: Person, index: number) {
+                    FamilyDetailOption.pushPerson(child, 'children', me,
+                        child.six == "male" ? "儿子" : "女儿", 0, 200 + index * 150);
                 });
         }
 
